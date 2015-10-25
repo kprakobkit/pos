8bc13a4f7fdb9ca88ccb9aa654c237f069df5c8d
achiu8 <achiu8@gmail.com>
2015-10-25 00:37:41 -0700

    Fix path to dist directory

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ import makeStore from './store';
 var app = express();
 const isProduction = process.env.NODE_ENV === 'production';
 var port = isProduction ? process.env.PORT : 3000;
-const distPath = path.resolve(__dirname, 'dist');
+const distPath = path.resolve(__dirname, '..', 'dist');
 const io = new Server().attach(8090);
 const store = makeStore();
 
