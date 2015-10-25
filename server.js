222d85570893d64c924c6704b93a0613e89834d2
kprakobkit <kprakobkit@gmail.com>
2015-10-24 17:34:09 -0700

    Add express to serve index.html

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 import Server from 'socket.io';
+import express from 'express';
+import path from 'path';
 
-const io = new Server().attach(process.env.PORT || 8090);
+var app = express();
+const isProduction = process.env.NODE_ENV === 'production';
+var port = isProduction ? process.env.PORT : 3000;
+const distPath = path.resolve(__dirname, 'dist');
+const io = new Server().attach(8090);
 
 console.log('Server running...');
+
+app.use(express.static(distPath));
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
 
 io.on('connection', (socket) => {
   console.log('connected!');
