03a89acd2de63b5aa7a214fba821c6249dd73cdf
achiu8 <achiu8@gmail.com>
2015-10-27 00:01:50 -0700

    Home too

--- test/components/Home_spec.js
+++ test/components/Home_spec.js
@@ -2,14 +2,14 @@ import { expect } from 'chai';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import Home from '../../src/components/Home';
+import HomeComponent from '../../src/components/Home';
 
 const { renderIntoDocument, findRenderedDOMComponentWithClass } = TestUtils;
-const home = React.createFactory(Home);
+const Home = React.createFactory(HomeComponent);
 
 describe('Home', () => {
   it('renders link to orders page', () => {
-    const component = renderIntoDocument(home());
+    const component = renderIntoDocument(Home());
     const link = findRenderedDOMComponentWithClass(component, 'orders-link');
 
     expect(link.textContent).to.equal('Orders');
