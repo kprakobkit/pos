f95e14b3fc26a0a2b5e5405a7b421e9c7ef61f0d
achiu8 <achiu8@gmail.com>
2015-10-27 00:01:14 -0700

    Import as component, more shorthands, remove multi-spaces

--- test/components/Order_spec.js
+++ test/components/Order_spec.js
@@ -2,14 +2,14 @@ import { expect } from 'chai';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import Order from '../../src/components/Order';
+import OrderComponent from '../../src/components/Order';
 
 const {
   renderIntoDocument,
   findRenderedDOMComponentWithClass,
   Simulate
 } = TestUtils;
-const order = React.createFactory(Order);
+const Order = React.createFactory(OrderComponent);
 
 describe('Order', () => {
   const id = 1;
@@ -17,16 +17,16 @@ describe('Order', () => {
   const toggleOrder = () => {};
 
   it('renders id', () => {
-    const orderProps = { id: id, status: status, toggleOrder: toggleOrder };
-    const component = renderIntoDocument(order(orderProps));
+    const props = { id, status, toggleOrder };
+    const component = renderIntoDocument(Order(props));
     const orderId = findRenderedDOMComponentWithClass(component, 'order-number');
 
     expect(orderId.textContent).to.contain(id.toString());
   });
 
   it('renders status', () => {
-    const orderProps = { id: id, status: status, toggleOrder: toggleOrder };
-    const component = renderIntoDocument(order(orderProps));
+    const props = { id, status, toggleOrder };
+    const component = renderIntoDocument(Order(props));
     const orderStatus = findRenderedDOMComponentWithClass(component, 'order-status');
 
     expect(orderStatus.textContent).to.contain(status);
@@ -35,8 +35,8 @@ describe('Order', () => {
   it('calls callback when button clicked', () => {
     let toggleOrderCalled = false;
     const toggleOrderStub = () => toggleOrderCalled = true;
-    const orderProps = { id: id, status: status, toggleOrder: toggleOrderStub };
-    const component = renderIntoDocument(order(orderProps));
+    const props = { id, status, toggleOrder: toggleOrderStub };
+    const component = renderIntoDocument(Order(props));
     const button = findRenderedDOMComponentWithClass(component, 'order-status-toggle');
 
     Simulate.click(button);
--- test/components/Orders_spec.js
+++ test/components/Orders_spec.js
@@ -1,9 +1,9 @@
-import { expect }    from 'chai';
-import React         from 'react';
-import ReactDOM      from 'react-dom';
-import TestUtils     from 'react-addons-test-utils';
+import { expect } from 'chai';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
 import { List, Map } from 'immutable';
-import Orders        from '../../src/components/Orders';
+import OrdersComponent from '../../src/components/Orders';
 
 const {
   renderIntoDocument,
@@ -11,13 +11,13 @@ const {
   scryRenderedDOMComponentsWithClass
 } = TestUtils;
 
-const orders = React.createFactory(Orders);
+const Orders = React.createFactory(OrdersComponent);
 
 describe('Orders', () => {
   const toggleOrder = () => {};
 
   it('renders title', () => {
-    const component = renderIntoDocument(orders({ orders: List(), toggleOrder: toggleOrder }));
+    const component = renderIntoDocument(Orders({ orders: List(), toggleOrder }));
     const title = findRenderedDOMComponentWithClass(component, 'orders-title');
 
     expect(title.textContent).to.equal('Orders');
@@ -27,15 +27,15 @@ describe('Orders', () => {
     const order1 = Map({ id: 1, status: 'open' });
     const order2 = Map({ id: 2, status: 'open' });
     const order3 = Map({ id: 3, status: 'open' });
-    const ordersProp = List.of(order1, order2, order3);
-    const component = renderIntoDocument(orders({ orders: ordersProp, toggleOrder: toggleOrder }));
+    const orders = List.of(order1, order2, order3);
+    const component = renderIntoDocument(Orders({ orders, toggleOrder }));
     const children = scryRenderedDOMComponentsWithClass(component, 'order');
 
     expect(children.length).to.equal(3);
   });
 
   it('renders message if there are no orders', () => {
-    const component = renderIntoDocument(orders({ orders: List(), toggleOrder: toggleOrder }));
+    const component = renderIntoDocument(Orders({ orders: List(), toggleOrder }));
     const ordersComponent = findRenderedDOMComponentWithClass(component, 'orders-message');
 
     expect(ordersComponent.textContent).to.equal('There are currently no orders.');
