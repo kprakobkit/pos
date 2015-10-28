fd31675e60ea019a048d73ee05acf158b336fed9
achiu8 <achiu8@gmail.com>
2015-10-27 22:11:09 -0700

    Remove unnecessary values

--- server/action_creators.js
+++ server/action_creators.js
@@ -11,14 +11,13 @@ export function setState(state) {
 
 export function toggleOrder(id) {
   return {
-    meta: { remote: true },
     type: constants.TOGGLE_ORDER,
     id
   };
 }
 
 export function loadOrders() {
-  return (dispatch, getState) => {
+  return (dispatch) => {
     return Order.find({}, (err, response) => {
       let orders = response.map((order) => {
         return {
