85128a7b5bc0489f1e267246e5d7eadf4e6a6aa6
achiu8 <achiu8@gmail.com>
2015-10-30 00:57:29 -0700

    Create OrdersFilter component

---
+++ src/components/OrdersFilter.js
@@ -0,0 +1,37 @@
+import { Component, PropTypes, DOM as dom, createFactory } from 'react';
+import actions from '../action_creators';
+import constants from '../constants';
+
+class OrdersFilter extends Component {
+  render() {
+    return (
+      dom.div(
+        null,
+        dom.button(
+          {
+            className: 'orders-filter-option orders-filter-all',
+            onClick:   () => this.props.filterOrders(constants.ALL)
+          },
+          'All'
+        ),
+        dom.button(
+          {
+            className: 'orders-filter-option orders-filter-open',
+            onClick:   () => this.props.filterOrders(constants.OPEN)
+          },
+          'Open'
+        ),
+        dom.button(
+          {
+            className: 'orders-filter-option orders-filter-ready',
+            onClick:   () => this.props.filterOrders(constants.READY_FOR_BILL)
+          },
+          'Ready for Bill'
+        ),
+      )
+    );
+  }
+}
+
+export default OrdersFilter;
+
