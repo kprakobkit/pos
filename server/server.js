c99577bc1e0d12ac66d6a08d5260a8843f5c0fe9
kprakobkit <kprakobkit@gmail.com>
2015-11-03 23:06:58 -0800

    Separate seed file.

--- server/server.js
+++ server/server.js
@@ -4,9 +4,7 @@ import path from 'path';
 import mongoose from 'mongoose';
 import makeStore from './store';
 import socketEvents from './socket_events';
-import constants from '../src/constants';
-import Order from '../models/order';
-import Item from '../models/item';
+import seedData from './seed';
 
 const app = express();
 const isProduction = process.env.NODE_ENV === 'production';
@@ -39,51 +37,5 @@ io.on('connection', attachSocketEvents);
 store.subscribe(
   () => io.emit('state', store.getState())
 );
-
-// seeding for dev
-function seedData() {
-  Order.remove({}, () => {
-    console.log('Orders collection dropped');
-  });
-
-  Item.remove({}, () => {
-    console.log('Items collection dropped');
-  });
-
-  Item({
-    name: 'Pho',
-    price: 1050
-  }).save((err, item) => {
-    Order({
-      id: '18',
-      status: constants.CLOSED,
-      items: item._id
-    }).save();
-  });
-
-  Item({
-    name: 'Bun Bo Hue',
-    price: 1125
-  }).save((err, item) => {
-    Order({
-      id: '15',
-      status: constants.CLOSED,
-      items: item._id
-    }).save();
-
-    Order({
-      id: '16',
-      status: constants.OPEN,
-      items: item._id
-    }).save();
-
-    Order({
-      id: '17',
-      status: constants.READY_FOR_BILL,
-      items: item._id
-    }).save();
-  });
-
-}
 
 seedData();
