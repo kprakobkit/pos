92fa9f6c727a400bfe5ea4f06f8a48faa9571c3c
kprakobkit <kprakobkit@gmail.com>
2015-11-04 11:59:21 -0800

    Modify seed file to use async.

--- server/seed.js
+++ server/seed.js
@@ -1,49 +1,63 @@
 import constants from '../src/constants';
 import Order from '../models/order';
 import Item from '../models/item';
+import async from 'async';
 
-// seeding for dev
+function removeData(next) {
+  async.parallel([
+    () => {
+      Order.remove({}, () => {
+        console.log('Orders collection dropped');
+      });
+    },
+    () => {
+      Item.remove({}, () => {
+        console.log('Items collection dropped');
+      });
+    }
+  ]);
+
+  next();
+}
+
+function createItem(itemAttr, cb) {
+  Item({
+    name: itemAttr.name,
+    price: itemAttr.price
+  }).save(cb);
+}
+
+function createOrder(orderAttr, cb) {
+  Order({
+    id: orderAttr.id,
+    status: orderAttr.status,
+    items: orderAttr.items._id
+  }).save(cb);
+}
+
+function createItems(cb) {
+  async.parallel([
+    async.apply(createItem, { name: 'Pho', price: 1050 }),
+    async.apply(createItem, { name: 'Rice', price: 925 })
+  ], cb);
+}
+
+function createOrders(items, cb) {
+  async.parallel([
+    async.apply(createOrder, { id: '15', status: constants.OPEN, items: items[0][0] }),
+    async.apply(createOrder, { id: '16', status: constants.CLOSED, items: items[1][0] }),
+    async.apply(createOrder, { id: '17', status: constants.READY_FOR_BILL, items: items[0][0] }),
+    async.apply(createOrder, { id: '18', status: constants.OPEN, items: items[1][0] })
+  ], cb);
+}
+
 function seedData() {
-  Order.remove({}, () => {
-    console.log('Orders collection dropped');
-  });
-
-  Item.remove({}, () => {
-    console.log('Items collection dropped');
-  });
-
-  Item({
-    name: 'Pho',
-    price: 1050
-  }).save((err, item) => {
-    Order({
-      id: '18',
-      status: constants.CLOSED,
-      items: item._id
-    }).save();
-  });
-
-  Item({
-    name: 'Bun Bo Hue',
-    price: 1125
-  }).save((err, item) => {
-    Order({
-      id: '15',
-      status: constants.CLOSED,
-      items: item._id
-    }).save();
-
-    Order({
-      id: '16',
-      status: constants.OPEN,
-      items: item._id
-    }).save();
-
-    Order({
-      id: '17',
-      status: constants.READY_FOR_BILL,
-      items: item._id
-    }).save();
+  async.waterfall([
+    async.apply(removeData),
+    async.apply(createItems),
+    async.apply(createOrders)
+  ], function (err, results) {
+    console.log('Completed seeding data...');
   });
 }
 
