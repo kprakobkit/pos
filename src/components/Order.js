28402cd4e14ffb1f40557e74b9be50a07ea33af8
achiu8 <achiu8@gmail.com>
2015-11-04 20:46:26 -0800

    Some more styling of orders list

--- src/components/Order.js
+++ src/components/Order.js
@@ -10,16 +10,24 @@ class Order extends Component {
 
   render() {
     return (
-      dom.div(
-        { className: 'order row' },
-        Link(
-          {
-            to:        `/orders/${this.props.id}`,
-            className: 'order-number col-xs-6'
-          },
-          `Order ${this.props.id}`
-        ),
-        dom.div({ className: 'order-status text-capitalize col-xs-6' }, this.printStatus())
+      Link(
+        {
+          to:        `/orders/${this.props.id}`,
+          className: 'order'
+        },
+        dom.div(
+          { className: 'row' },
+          dom.div(
+            { className: 'order-number col-xs-6' },
+            `Order ${this.props.id}`
+          ),
+          dom.div(
+            {
+              className: 'order-status col-xs-6 text-capitalize text-center'
+            },
+            this.printStatus()
+          )
+        )
       )
     );
   }
