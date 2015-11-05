b3a988df2c169f19cc62b11db876a8b4e5d38fdc
kprakobkit <kprakobkit@gmail.com>
2015-11-05 15:23:28 -0800

    Add selected item to an order on the new order page.

--- src/components/NewOrder.js
+++ src/components/NewOrder.js
@@ -4,27 +4,58 @@ import * as actions from '../action_creators';
 
 const mapStateToProps = function (state) {
   return {
-    orders: state.orders,
     items: state.items
   };
 };
 
 class NewOrder extends Component {
-  componentWillMount() {
+  constructor(props) {
+    super(props);
+    this.selectItem = this.selectItem.bind(this);
+    this.addOrderItem = this.addOrderItem.bind(this);
+    this.state = {
+      items: []
+    };
+  }
+
+  componentWillMount(event) {
     this.props.loadItems();
+  }
+
+  addOrderItem() {
+    const defaultValue = this.props.items[0] && this.props.items[0].id;
+    this.state.items.push(this.state.selectedItem || defaultValue);
+  }
+
+  selectItem(event) {
+    this.state.selectedItem = event.target.value;
   }
 
   render() {
     return dom.div(
       null,
       dom.h1(null, 'New Order Page'),
+      dom.select(
+        { id: 'item', onChange: this.selectItem },
+        this.props.items.map((item) => {
+          return dom.option({ value: item.id }, item.name);
+        })
+      ),
       dom.button({
           className: 'btn btn-default',
-          onClick: this.props.addOrder
+          onClick: this.addOrderItem
+        }, 'Add Item'),
+      dom.br(),
+      dom.button({
+          className: 'btn btn-primary'
         }, 'Submit')
     );
   }
 }
+
+NewOrder.defaultProps = {
+  items: []
+};
 
 export default NewOrder;
 export const NewOrderContainer = connect(mapStateToProps, actions)(NewOrder);
