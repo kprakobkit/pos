c0e7a0c6627fb9d235cadcacf75f90a82c144ee1
achiu8 <achiu8@gmail.com>
2015-11-04 21:23:01 -0800

    Loop in render

--- src/components/OrdersFilter.js
+++ src/components/OrdersFilter.js
@@ -5,37 +5,35 @@ import constants from '../constants';
 class OrdersFilter extends Component {
   constructor(props) {
     super(props);
-    this.renderFilters = this.renderFilters.bind(this);
+    this.renderFilter = this.renderFilter.bind(this);
   }
 
   isSelectedFilter(filter) {
     return this.props.filter === filter ? 'btn-primary' : 'btn-default';
   }
 
-  renderFilters() {
-    return [constants.ALL, constants.OPEN, constants.READY_FOR_BILL].map((filter) => {
-      const normalized = filter.toLowerCase().split('_')[0];
-      return (
-        dom.button(
-          {
-            key: filter,
-            className: [
-              `orders-filter-option orders-filter-${normalized}`,
-              `btn ${this.isSelectedFilter(filter)} text-capitalize`
-            ].join(' '),
-            onClick: () => this.props.filterOrders(filter)
-          },
-          this.props.printOrderStatus(filter)
-        )
-      );
-    });
+  renderFilter(filter) {
+    const normalized = filter.toLowerCase().split('_')[0];
+    return (
+      dom.button(
+        {
+          key: filter,
+          className: [
+            `orders-filter-option orders-filter-${normalized}`,
+            `btn ${this.isSelectedFilter(filter)} text-capitalize`
+          ].join(' '),
+          onClick: () => this.props.filterOrders(filter)
+        },
+        this.props.printOrderStatus(filter)
+      )
+    );
   }
 
   render() {
     return (
       dom.div(
         { className: 'text-center' },
-        this.renderFilters()
+        [constants.ALL, constants.OPEN, constants.READY_FOR_BILL].map(this.renderFilter)
       )
     );
   }
