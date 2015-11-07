41683481945a4118e01a3f4505eee297f8611ca0
kprakobkit <kprakobkit@gmail.com>
2015-11-07 09:36:28 -0800

    Use const instead of let.

--- models/item.js
+++ models/item.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-let itemSchema = new mongoose.Schema({
+const itemSchema = new mongoose.Schema({
   name: String,
   price: Number // apparently mongo doesn't support decimal so will have store dollar amount in cents...
 });
