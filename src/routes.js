3939c4218d5e0777d16a4888adb23751c851cbd1
kprakobkit <kprakobkit@gmail.com>
2015-11-09 21:36:49 -0800

    Display order entries on the order details page.

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ import { Route as RouteComponent } from 'react-router';
 import App from './components/App';
 import Home from './components/Home';
 import { OrdersContainer } from './components/Orders';
-import OrderDetails from './components/OrderDetails';
+import { OrderDetailsContainer } from './components/OrderDetails';
 import { NewOrderContainer } from './components/NewOrder';
 
 const Route = React.createFactory(RouteComponent);
@@ -11,7 +11,7 @@ const Route = React.createFactory(RouteComponent);
 export default Route(
   { component: App },
   Route({ path: '/orders/new', component: NewOrderContainer }),
-  Route({ path: '/orders/:id', component: OrderDetails }),
+  Route({ path: '/orders/:id', component: OrderDetailsContainer }),
   Route({ path: '/orders', component: OrdersContainer }),
   Route({ path: '/', component: Home })
 );
--- test/components/OrderDetails_spec.js
+++ test/components/OrderDetails_spec.js
@@ -4,19 +4,52 @@ import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
 import OrderDetailsComponent from '../../src/components/OrderDetails';
 
-const { renderIntoDocument, findRenderedDOMComponentWithClass } = TestUtils;
+const {
+  renderIntoDocument,
+  findRenderedDOMComponentWithClass,
+  scryRenderedDOMComponentsWithClass
+} = TestUtils;
 const OrderDetails = React.createFactory(OrderDetailsComponent);
 
 describe('OrderDetails', () => {
-  const id = 1;
-  const params = { id };
-  const props = { params };
-
   it('renders title with correct order number from params', () => {
+    const id = 1;
+    const params = { id };
+    const props = { orders: [{ status: 'open', id: 1, entries: [] }], params };
     const component = renderIntoDocument(OrderDetails(props));
     const title = findRenderedDOMComponentWithClass(component, 'order-title');
 
     expect(title.textContent).to.contain(id.toString());
   });
+
+  it('renders the order status', () => {
+    const props = { orders: [{ status: 'open', id: 1, entries: [] }], params: { id: 1 } };
+    const component = renderIntoDocument(OrderDetails(props));
+    const status = findRenderedDOMComponentWithClass(component, 'order-status');
+
+    expect(status.textContent).to.contain('open');
+  });
+
+  it('renders the order entries', () => {
+    const entries = [
+      { name: 'rice', price: 1050, comment: 'brown rice', status: 'OPEN' },
+      { name: 'egg', price: 150, comment: 'sunny side up', status: 'DELIVERED' }
+    ];
+    const props = { orders: [{ status: 'open', id: 1, entries }], params: { id: 1 } };
+    const component = renderIntoDocument(OrderDetails(props));
+    const orderEntries = scryRenderedDOMComponentsWithClass(component, 'order-entry');
+    const entryNames = scryRenderedDOMComponentsWithClass(component, 'entry-name');
+    const entryComments = scryRenderedDOMComponentsWithClass(component, 'entry-comment');
+    const entryStatuses = scryRenderedDOMComponentsWithClass(component, 'entry-status');
+
+    expect(orderEntries.length).to.equal(2);
+    expect(entryNames[0].textContent).to.contain('rice');
+    expect(entryNames[1].textContent).to.contain('egg');
+    expect(entryComments[0].textContent).to.contain('brown rice');
+    expect(entryComments[1].textContent).to.contain('sunny side up');
+    expect(entryStatuses[0].textContent).to.contain('OPEN');
+    expect(entryStatuses[1].textContent).to.contain('DELIVERED');
+  });
+
 });
 
