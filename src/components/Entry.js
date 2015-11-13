047f314ec70868810bbd63862ab4891dad4e9b23
achiu8 <achiu8@gmail.com>
2015-11-12 22:50:45 -0800

    Create and refactor OrderDetails to use Entry component

---
+++ src/components/Entry.js
@@ -0,0 +1,36 @@
+import { Component, PropTypes, DOM as dom } from 'react';
+import { connect } from 'react-redux';
+import actions from '../action_creators';
+import constants from '../constants';
+import _ from 'underscore';
+
+class Entry extends Component {
+  render() {
+    return dom.tr(
+      { className: 'order-entry' },
+      dom.td({ className: 'entry-name' }, dom.h2(null, this.props.name)),
+      dom.td({ className: 'entry-comment' }, dom.h3(null, this.props.comment)),
+      dom.td(
+        { className: 'entry-status' },
+        dom.select(
+          { className: 'form-control input-lg', value: this.props.status },
+          dom.option({ value: constants.OPEN }, constants.OPEN),
+          dom.option({ value: constants.DELIVERED }, constants.DELIVERED),
+          dom.option({ value: constants.CANCELED }, constants.CANCELED)
+        )
+      )
+    );
+  }
+}
+
+Entry.propTypes = {
+  name: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  comment: PropTypes.string
+};
+
+Entry.defaultProps = {
+  comment: ''
+};
+
+export default Entry;
---
+++ test/components/Entry_spec.js
@@ -0,0 +1,43 @@
+import { expect } from 'chai';
+import { Component, DOM as dom, createFactory } from 'react';
+import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
+import constants from '../../src/constants';
+import EntryComponent from '../../src/components/Entry';
+
+const {
+  renderIntoDocument,
+  findRenderedDOMComponentWithClass,
+  Simulate
+} = TestUtils;
+const Entry = createFactory(EntryComponent);
+
+class TableComponent extends Component {
+  render() {
+    return dom.table(
+      null,
+      dom.tbody(
+        null,
+        Entry(this.props)
+      )
+    );
+  };
+}
+
+const Table = createFactory(TableComponent);
+
+describe('Entry', () => {
+  const name = 'Pho';
+  const status = constants.OPEN;
+  const comment = 'extra soup';
+  const props = { name, status, comment };
+  const component = renderIntoDocument(Table(props));
+
+  it('renders name and comment', () => {
+    const entryName = findRenderedDOMComponentWithClass(component, 'entry-name');
+    const entryComment = findRenderedDOMComponentWithClass(component, 'entry-comment');
+
+    expect(entryName.textContent).to.equal(name);
+    expect(entryComment.textContent).to.equal(comment);
+  });
+});
