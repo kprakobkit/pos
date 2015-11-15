3ffe37bb58968d7681e3f0b4fdb7a9ac7f59a519
kprakobkit <kprakobkit@gmail.com>
2015-11-15 15:55:31 -0800

    Move test from new order sepc to master items spec.

--- test/components/New_Order_spec.js
+++ test/components/New_Order_spec.js
@@ -26,61 +26,6 @@ describe('New Order', () => {
     component = renderIntoDocument(NewOrder({ masterItems, loadItems, addOrder }));
   });
 
-  it('adds an entry to the list each time', () => {
-    const addEntry = findRenderedDOMComponentWithClass(component, 'add-entry');
-    const addCommentFld = findRenderedDOMComponentWithClass(component, 'add-comment');
-
-    Simulate.click(addEntry);
-
-    const entries = scryRenderedDOMComponentsWithClass(component, 'entries');
-    const entryName = scryRenderedDOMComponentsWithClass(component, 'entry-name');
-    expect(entries.length).to.equal(1);
-    expect(entryName[0].textContent).to.equal(food.name);
-  });
-
-  it('adds a new comment for each entry', () => {
-    const addEntry = findRenderedDOMComponentWithClass(component, 'add-entry');
-    const addCommentFld = findRenderedDOMComponentWithClass(component, 'add-comment');
-
-    Simulate.change(addCommentFld, { target: { value: 'comment' } });
-    Simulate.click(addEntry);
-    Simulate.change(addCommentFld, { target: { value: 'new comment' } });
-    Simulate.click(addEntry);
-
-    const entryComment = scryRenderedDOMComponentsWithClass(component, 'entry-comment');
-
-    expect(entryComment[0].textContent).to.equal('comment');
-    expect(entryComment[1].textContent).to.equal('new comment');
-  });
-
-  it('clears the comment after adding an entry', () => {
-    const addEntry = findRenderedDOMComponentWithClass(component, 'add-entry');
-    let addCommentFld = findRenderedDOMComponentWithClass(component, 'add-comment');
-
-    Simulate.change(addCommentFld, { target: { value: 'no meat' } });
-    addCommentFld = findRenderedDOMComponentWithClass(component, 'add-comment');
-
-    expect(addCommentFld.value).to.equal('no meat');
-    Simulate.click(addEntry);
-
-    addCommentFld = findRenderedDOMComponentWithClass(component, 'add-comment');
-    expect(addCommentFld.value).to.equal('');
-  });
-
-  it('defaults to the first item in the list if not selected', () => {
-    const addEntry = findRenderedDOMComponentWithClass(component, 'add-entry');
-    const selectItems = findRenderedDOMComponentWithClass(component, 'select-items');
-    const submitOrder = findRenderedDOMComponentWithClass(component, 'submit-order');
-
-    Simulate.click(addEntry);
-    Simulate.click(submitOrder);
-
-    const entries = scryRenderedDOMComponentsWithClass(component, 'entries');
-    const entryName = scryRenderedDOMComponentsWithClass(component, 'entry-name');
-    expect(entries.length).to.equal(1);
-    expect(entryName[0].textContent).to.equal(food.name);
-  });
-
   it('removes an item from an order', () => {
     const addEntry = findRenderedDOMComponentWithClass(component, 'add-entry');
     const selectItems = findRenderedDOMComponentWithClass(component, 'select-items');
