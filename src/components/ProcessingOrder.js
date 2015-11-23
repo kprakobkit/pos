e0d2f7114c25c0360b70919b145e7c4105b2f7af
achiu8 <achiu8@gmail.com>
2015-11-22 16:15:03 -0800

    Create ProcessingOrder component

---
+++ src/components/ProcessingOrder.js
@@ -0,0 +1,47 @@
+import { Component, PropTypes, DOM as dom, createFactory } from 'react';
+import { connect } from 'react-redux';
+import constants from '../constants';
+import _ from 'underscore';
+import EntryComponent from './Entry';
+
+const Entry = createFactory(EntryComponent);
+
+class ProcessingOrder extends Component {
+  constructor(props) {
+    super(props);
+    this.renderEntry = this.renderEntry.bind(this);
+  }
+
+  renderEntry(entry, i) {
+    return entry.status !== constants.CANCELED ?
+      Entry(
+        _.extend({}, entry, {
+          key: i,
+          index: i,
+          changeEntryStatus: this.props.changeEntryStatus
+        })
+      ) :
+      null;
+  }
+
+  render() {
+    return (
+      dom.div(
+        { className: 'order-entries' },
+        dom.table(
+          { className: 'table table-striped' },
+          dom.tbody(
+            null,
+            this.props.order.entries.map(this.renderEntry)
+          )
+        )
+      )
+    );
+  }
+}
+
+ProcessingOrder.propTypes = {
+  order: PropTypes.object.isRequired
+};
+
+export default ProcessingOrder;
