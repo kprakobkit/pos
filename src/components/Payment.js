aac5fadee6137826e767cc1d7668b3e00d055f6c
achiu8 <achiu8@gmail.com>
2015-11-25 20:23:31 -0800

    Factor out rendering of input fields; don't include tip field for now

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,38 +1,36 @@
 import { Component, PropTypes, DOM as dom, createFactory } from 'react';
 
 class Payment extends Component {
+  constructor(props) {
+    super(props);
+    this.renderAmountField = this.renderAmountField.bind(this);
+  }
+
+  renderAmountField(label) {
+    return dom.div(
+      { className: 'form-group', key: label },
+      dom.label({ className: 'control-label col-xs-6 h4' }, label),
+      dom.div(
+        { className: 'col-xs-6' },
+        dom.input(
+          {
+            className: 'form-control input-lg text-right',
+            type: 'number'
+          }
+        )
+      )
+    );
+  }
+
   render() {
     return (
       dom.div(
         { className: 'payment-component' },
         dom.h2({ className: 'payment-component-heading' }, 'Amounts Paid'),
         dom.form(
-          { className: 'payment-form form-horizontal' },
-          dom.div(
-            { className: 'form-group' },
-            dom.label({ className: 'control-label col-xs-6 h4' }, 'Cash'),
-            dom.div(
-              { className: 'col-xs-6' },
-              dom.input({ className: 'form-control input-lg text-right', type: 'number' })
-            )
-          ),
-          dom.div(
-            { className: 'form-group' },
-            dom.label({ className: 'control-label col-xs-6 h4' }, 'Credit'),
-            dom.div(
-              { className: 'col-xs-6' },
-              dom.input({ className: 'form-control input-lg text-right', type: 'number' })
-            )
-          ),
-          dom.div(
-            { className: 'form-group' },
-            dom.label({ className: 'control-label col-xs-6 h4' }, 'Tip in Credit'),
-            dom.div(
-              { className: 'col-xs-6' },
-              dom.input({ className: 'form-control input-lg text-right', type: 'number' })
-            )
-          )
-        )
+          { className: 'payment-form form-horizontal', ref: 'form' },
+          ['Cash', 'Credit'].map(this.renderAmountField),
+        ),
       )
     );
   }
