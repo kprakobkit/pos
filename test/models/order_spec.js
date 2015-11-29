c24595b189a28a09664a48fbce44e5f44713c026
kprakobkit <kprakobkit@gmail.com>
2015-11-28 21:04:18 -0800

    Convert clear db to be a promise, and fix mongoose unit test to show failed assertions.

--- test/models/order_spec.js
+++ test/models/order_spec.js
@@ -6,43 +6,36 @@ import faker from 'faker';
 import constants from '../../src/constants';
 
 describe('Order', () => {
-  it ('defautls to OPEN status', function (done) {
-    Order({
-      id: faker.random.number()
-    }).save((err, newOrder) => {
+  it ('defautls to OPEN status', function () {
+    return Order().save()
+    .then((newOrder) => {
       expect(newOrder.status).to.equal(constants.OPEN);
-      done();
     });
   });
 
-  describe('get entries', (done) => {
-    beforeEach((done) => {
-      Item({
+  describe('get entries', () => {
+    beforeEach(() => {
+      return Item({
         name: 'Rice',
         price: 1000
-      }).save(done);
+      }).save();
     });
 
-    it('adds createdAt to each entry', (done) => {
-      Item.findOne({ name: 'Rice' })
+    it('adds createdAt to each entry', () => {
+      return Item.findOne({ name: 'Rice' })
       .then((item) => Order.addOrder([item]))
       .then((order) => {
         expect(order.entries[0].createdAt).to.exist;
-        done();
       });
     });
   });
 
-  it ('updateStatus', function (done) {
-    Order({
-      id: faker.random.number()
-    }).save((err, newOrder) => {
-      Order.updateStatus(newOrder.id, constants.READY_FOR_BILL).then(() => {
-        Order.findOne({ id: newOrder.id }).then((order) => {
-          expect(order.status).to.equal(constants.READY_FOR_BILL);
-          done();
-        });
-      });
+  it ('updateStatus', function () {
+    return Order({ id: 1 }).save()
+    .then(() => Order.updateStatus(1, constants.READY_FOR_BILL))
+    .then(() => Order.findOne({ id: 1 }))
+    .then((order) => {
+      expect(order.status).to.equal(constants.READY_FOR_BILL);
     });
   });
 });
