e53a0c1efc5b01f2edc0e128902c86ae1c155271
kprakobkit <kprakobkit@gmail.com>
2015-11-28 18:06:39 -0800

    Async makes me sad :( Use promise instead.

--- server/seed.js
+++ server/seed.js
@@ -2,7 +2,6 @@ import constants from '../src/constants';
 import Order from '../models/order';
 import Item from '../models/item';
 import Entry from '../models/entry';
-import async from 'async';
 import mongoose from 'mongoose';
 import faker from 'faker';
 import _ from 'underscore';
@@ -16,93 +15,90 @@ const orderStatuses = [
 
 mongoose.connect(process.env.MONGOLAB_URI || config.developmentDB);
 
-function removeData(next) {
-  async.parallel([
-    () => {
-      console.log('Dropping orders collection');
-
-      Order.remove({}, (err, res) => {
+function removeData() {
+  const collections = mongoose.connection.collections;
+  const removeCollectionsPromise = _.map(collections, (collection) => {
+    return new Promise((resolve, reject) => {
+      collection.remove((err) => {
         if(err) {
-          console.error(`Error dropping the orders colection. ${err}`);
+          console.error(`Error removing ${collection.name} collection`);
+          reject(err);
         }
 
-        console.log('Orders collection dropped');
+        console.log(`Removed ${collection.name} collection`);
+        resolve();
       });
-    },
-    () => {
-      console.log('Dropping items collection');
+    });
+  });
 
-      Item.remove({}, (err, res) => {
-        if(err) {
-          console.error(`Error dropping the items colection. ${err}`);
-        }
-
-        console.log('Items collection dropped');
-      });
-    }
-  ]);
-
-  next();
+  return Promise.all(removeCollectionsPromise);
 }
 
 function createItem(name, price, cb) {
-  Item({ name, price }).save((err, result) => {
-    if(err) {
-      console.error(`Item name: ${name}`);
-      console.error(`Error creating item. ${err}`);
-    }
+  return new Promise((resolve, reject) => {
+    Item({ name, price }).save((err, result) => {
+      if(err) {
+        console.error(`Error creating item ${name}. ${err}`);
+        reject(err);
+      }
 
-    console.log(`Successfully created ${name}`);
-    cb(err, result);
+      resolve();
+    });
   });
 }
 
-function createOrder(items, cb) {
-  Order({
-    id: faker.random.number(),
-    status: _.sample(orderStatuses),
-    entries: items.map(toEntry)
-  }).save((err, result) => {
-    if(err) {
-      console.error(`Error creating order. ${err}`);
-    }
+function createOrder(items, i) {
+  return new Promise((resolve, reject) => {
+    Order({
+      id: faker.random.number(),
+      status: _.sample(orderStatuses),
+      entries: items.map(toEntry)
+    }).save((err, result) => {
+      if(err) {
+        console.error(`Error creating order. ${error}`);
+        reject(err);
+      }
 
-    console.log('Successfully created order.');
-    cb(result);
+      resolve();
+    });
   });
 }
 
 function createItems(cb) {
-  async.parallel([
-    async.apply(createItem, 'Pho', 1050),
-    async.apply(createItem, 'Burger', 925),
-    async.apply(createItem, 'Rice', 175),
-    async.apply(createItem, 'Dessert', 430)
-  ], cb);
+  return Promise.all([
+    createItem('Pho', 1050),
+    createItem('Burger', 925),
+    createItem('Rice', 175),
+    createItem('Dessert', 430)
+  ])
+  .then(() => {
+    console.log('Successfully created all items');
+  });
 }
 
-function createOrders(items, cb) {
-  async.parallel([
-    async.apply(createOrder, _.sample(items, 2)),
-    async.apply(createOrder, _.sample(items, 2)),
-    async.apply(createOrder, _.sample(items, 2)),
-    async.apply(createOrder, _.sample(items, 2))
-  ], cb);
+function createOrders(items) {
+  return Promise.all([
+    createOrder(_.sample(items, 2)),
+    createOrder(_.sample(items, 2)),
+    createOrder(_.sample(items, 2)),
+    createOrder(_.sample(items, 2))
+  ])
+  .then(() => {
+    console.log('Successfully created all orders');
+  });
 }
 
 function seedData() {
-  async.waterfall([
-    async.apply(removeData),
-    async.apply(createItems),
-    async.apply(createOrders)
-  ], function (err, results) {
-    if(err) {
-      console.log('Error seeding database, plese try again.');
-      process.exit(1);
-    }
-
+  removeData()
+  .then(createItems)
+  .then(createOrders)
+  .then(() => {
     console.log('Completed seeding database...');
     process.exit(0);
+  })
+  .catch((err) => {
+    console.log(`Error seeding database, plese try again. ${err}`);
+    process.exit(1);
   });
 }
 
