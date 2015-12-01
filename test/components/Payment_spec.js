bdb31c2c949bcb62492af1ab864a2fc58dc4f800
achiu8 <achiu8@gmail.com>
2015-11-30 20:28:11 -0800

    Add Payment form specs

--- test/components/Payment_spec.js
+++ test/components/Payment_spec.js
@@ -14,13 +14,14 @@ const {
 } = TestUtils;
 const Payment = React.createFactory(PaymentComponent);
 
-function setup() {
+function setup({ transaction } = {}) {
   const startingBalance = 2000;
   const setClosed = () => {};
-  const component = renderIntoDocument(Payment({ startingBalance, setClosed }));
+  const component = renderIntoDocument(Payment({ startingBalance, transaction, setClosed }));
 
   return {
     component,
+    transaction,
     startingBalance
   };
 }
@@ -58,6 +59,34 @@ describe('Payment', () => {
     Simulate.change(input);
 
     expect(balance.textContent).to.contain($.format(startingBalance));
+  });
+
+  it('prefills input fields with zero if no existing transaction amounts', () => {
+    const { component } = setup();
+    const balance = findRenderedDOMComponentWithClass(component, 'payment-balance-amount');
+    const cashInput = findRenderedDOMComponentWithClass(component, 'cash-amount-input');
+    const creditInput = findRenderedDOMComponentWithClass(component, 'credit-amount-input');
+    const tipInput = findRenderedDOMComponentWithClass(component, 'tip-amount-input');
+
+    expect(cashInput.value).to.eq($.dollars(0));
+    expect(creditInput.value).to.eq($.dollars(0));
+    expect(tipInput.value).to.eq($.dollars(0));
+  });
+
+  it('prefills input fields with existing transaction amounts', () => {
+    const cash = 1000;
+    const credit = 2000;
+    const tip = 500;
+    const existing = { cash, credit, tip };
+    const { component, transaction, startingBalance } = setup({ transaction: existing });
+    const balance = findRenderedDOMComponentWithClass(component, 'payment-balance-amount');
+    const cashInput = findRenderedDOMComponentWithClass(component, 'cash-amount-input');
+    const creditInput = findRenderedDOMComponentWithClass(component, 'credit-amount-input');
+    const tipInput = findRenderedDOMComponentWithClass(component, 'tip-amount-input');
+
+    expect(cashInput.value).to.eq($.dollars(cash));
+    expect(creditInput.value).to.eq($.dollars(credit));
+    expect(tipInput.value).to.eq($.dollars(tip));
   });
 });
 
