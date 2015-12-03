737dab212a8943e75d6d1c19a88354962e6c628d
achiu8 <achiu8@gmail.com>
2015-12-02 23:43:58 -0800

    Create Nav component

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import { Component, DOM as dom, createFactory } from 'react';
+import { Link as LinkComponent } from 'react-router';
+
+const Link = createFactory(LinkComponent);
+
+class Nav extends Component {
+  render() {
+    return (
+      dom.div(
+        { className: 'clearfix' },
+        dom.ul(
+          { className: 'nav nav-pills pull-right' },
+          dom.li(null, Link({ to: '/orders', className: 'nav-orders-link' }, dom.h4(null, 'Orders'))),
+          dom.li(null, Link({ to: '/chef', className: 'nav-chef-link' }, dom.h4(null, 'Chef')))
+        )
+      )
+    );
+  }
+}
+
+export default Nav;
