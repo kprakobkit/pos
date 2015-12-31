f0cbda96859e2b5ab46d2000ad452a1624c0e02a
kprakobkit <kprakobkit@gmail.com>
2015-12-30 19:09:57 -0800

    Refactor; only show add entries button for open orders.

--- src/components/OrderDetails.js
+++ src/components/OrderDetails.js
@@ -47,44 +47,57 @@ class OrderDetails extends Component {
     this.setState({ order });
   }
 
+  renderOrderInformation() {
+    return (
+      dom.div(
+        { className: 'col-xs-6' },
+        dom.h3(
+          { className: 'text-left order-information' },
+          `Table ${this.state.order.tableNumber} / Order ${this.props.params.id}  `,
+          dom.span({ className: 'text-info' }, `${this.state.order.status}`),
+          dom.small(
+            null,
+            this.state.order.status === constants.READY_FOR_BILL ? dom.button({
+              className: 'back-to-open btn btn-link',
+              onClick: this.props.setOpen.bind(null, this.props.params.id)
+            }, 'Back to "Open" Status') : null
+          )
+        )
+      )
+    );
+  }
+
+  renderAddEntriesButton() {
+    return (
+      dom.div(
+        { className: 'col-xs-6 text-right' },
+        dom.button(
+          { className: `toggle-add-entry btn btn-lg btn-${ this.state.showAddEntry ? 'danger' : 'info' }`, onClick: this.toggleForm },
+          this.state.showAddEntry ?
+            [dom.span({
+            className: 'glyphicon glyphicon-remove',
+            key: 'remove',
+            'aria-hidden': true
+          }), null] :
+            [dom.span({
+            className: 'glyphicon glyphicon-plus',
+            key: 'add',
+            'aria-hidden': true
+          }), '  Add More Items']
+        ),
+      )
+    );
+  }
+
   render() {
     return (
       dom.div(
         null,
         dom.div(
           { className: 'row' },
-          dom.div(
-            { className: 'col-xs-6' },
-            dom.h3(
-              { className: 'text-left order-information' },
-              `Table ${this.state.order.tableNumber} / Order ${this.props.params.id}  `,
-              dom.span({ className: 'text-info' }, `${this.state.order.status}`),
-              dom.small(
-                null,
-                this.state.order.status === constants.READY_FOR_BILL ? dom.button({
-                  className: 'back-to-open btn btn-link',
-                  onClick: this.props.setOpen.bind(null, this.props.params.id)
-                }, 'Back to "Open" Status') : null
-              )
-            )
-          ),
-          dom.div(
-            { className: 'col-xs-6 text-right' },
-            dom.button(
-              { className: `toggle-add-entry btn btn-lg btn-${ this.state.showAddEntry ? 'danger' : 'info' }`, onClick: this.toggleForm },
-              this.state.showAddEntry ?
-                [dom.span({
-                className: 'glyphicon glyphicon-remove',
-                key: 'remove',
-                'aria-hidden': true
-              }), null] :
-                [dom.span({
-                className: 'glyphicon glyphicon-plus',
-                key: 'add',
-                'aria-hidden': true
-              }), '  Add More Items']
-            ),
-          ),
+          this.renderOrderInformation(),
+          this.state.order.status === constants.OPEN ?
+            this.renderAddEntriesButton() : null
         ),
         this.state.order.status === constants.OPEN ?
           OpenOrder(
