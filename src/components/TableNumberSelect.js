cc80cfd18e4140d273f92dfbbad2bbfcab694d4a
kprakobkit <kprakobkit@gmail.com>
2016-01-06 20:26:34 -0800

    Refactor table number select into a component.

---
+++ src/components/TableNumberSelect.js
@@ -0,0 +1,23 @@
+import { Component, PropTypes, DOM as dom } from 'react';
+import constants from '../../src/constants';
+import _ from 'underscore';
+
+const tableNumbers = _.range(1, 41).concat('TOGO', 'BAR');
+
+class TableNumberSelect extends Component {
+  render() {
+    return (
+      dom.select(
+        { className: 'table-numbers input-lg form-control', value: this.props.tableNumber, onChange: this.props.handleOnChange },
+        tableNumbers.map((num) => dom.option({ key: num, className: 'table-number', value: num }, num))
+      )
+    );
+  }
+}
+
+TableNumberSelect.propTypes = {
+  tableNumber: PropTypes.number.isRequired,
+  handleOnChange: PropTypes.func.isRequired
+};
+
+export default TableNumberSelect;
