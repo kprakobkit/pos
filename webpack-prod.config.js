9fe5ec22370b038da8b82387143be1cd14afa5e8
kprakobkit <kprakobkit@gmail.com>
2016-04-02 20:31:02 -0700

    Removes webpack bootstrap.
    This reverts commit 4285b25b9db6f3685a6ea4872f66553b2adf7dd6.

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -2,37 +2,18 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'bootstrap-webpack!./bootstrap.config.js',
     './src/index.js'
   ],
   module: {
-    loaders: [
-      {
-        test:    /\.js$/,
-        exclude: /node_modules/,
-        loader:  'babel'
-      },
-      {
-        test: /\.css$/,
-        loader: 'style!css'
-      },
-      {
-        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/font-woff'
-      },
-      {
-        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream'
-      },
-      {
-        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'file'
-      },
-      {
-        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
-        loader: 'url?limit=10000&mimetype=image/svg+xml'
-      }
-    ]
+    loaders: [{
+      test:    /\.js$/,
+      exclude: /node_modules/,
+      loader:  'babel'
+    },
+    {
+      test: /\.css$/,
+      loader: 'style!css'
+    }]
   },
   resolve: {
     extensions: ['', '.js']
